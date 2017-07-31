1a39dc6ec9a5ed24c6942c603210ad12c0754e8c
Joe Huss <detain@interserver.net>
2017-07-30 23:35:24 -0400

    refactoring some calls

--- src/Plugin.php
+++ src/Plugin.php
@@ -66,7 +66,7 @@ class Plugin {
 	public static function loadProcessing(GenericEvent $event) {
 		$service = $event->getSubject();
 		$service->setModule(self::$module)
-			->set_enable(function($service) {
+			->setEnable(function($service) {
 				$serviceTypes = run_event('get_service_types', FALSE, self::$module);
 				$serviceInfo = $service->getServiceInfo();
 				$settings = get_module_settings(self::$module);
@@ -74,7 +74,7 @@ class Plugin {
 				$db->query("update {$settings['TABLE']} set {$settings['PREFIX']}_status='active' where {$settings['PREFIX']}_id='{$serviceInfo[$settings['PREFIX'].'_id']}'", __LINE__, __FILE__);
 				$GLOBALS['tf']->history->add(self::$module, 'change_status', 'active', $serviceInfo[$settings['PREFIX'].'_id'], $serviceInfo[$settings['PREFIX'].'_custid']);
 				admin_email_server_pending_setup($serviceInfo[$settings['PREFIX'].'_id']);
-			})->set_reactivate(function($service) {
+			})->setReactivate(function($service) {
 				$serviceTypes = run_event('get_service_types', FALSE, self::$module);
 				$serviceInfo = $service->getServiceInfo();
 				$settings = get_module_settings(self::$module);
@@ -90,7 +90,7 @@ class Plugin {
 				$headers .= 'Content-type: text/html; charset=UTF-8'.EMAIL_NEWLINE;
 				$headers .= 'From: '.TITLE.' <'.EMAIL_FROM.'>'.EMAIL_NEWLINE;
 				admin_mail($subject, $email, $headers, FALSE, 'admin_email_server_reactivated.tpl');
-			})->set_disable(function() {
+			})->setDisable(function() {
 			})->register();
 	}
 
