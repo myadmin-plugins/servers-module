0a3397cfae5e91145ca2766fa0f7bc7c5f27776d
Joe Huss <detain@interserver.net>
2022-07-22 12:00:50 -0400

    fixing minor problems

--- bin/update_ipmi.php
+++ bin/update_ipmi.php
@@ -11,7 +11,8 @@
 function update_ipmi_ip()
 {
 	require_once __DIR__.'/../../../../include/functions.inc.php';
-	$GLOBALS['tf']->session->create(160307, 'services', false, 0, false, substr(basename($_SERVER['argv'][0], '.php'), 0, 32));
+    if (isset($_SERVER['argv']))
+	    $GLOBALS['tf']->session->create(160307, 'services', false, 0, false, substr(basename($_SERVER['argv'][0], '.php'), 0, 32));
 	if (!isset($_SERVER['SSH_CLIENT']) && $GLOBALS['tf']->ima !== 'admin' && $GLOBALS['tf']->accounts->data['ima'] !== 'admin') {
 		die('You\'re not authorized');
 	}
